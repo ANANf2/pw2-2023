ffd9ebb25858ee42d4815386b21506d7096a5cd3
ANANf2 <anandsyahidah02@gmail.com>
2023-03-15 22:48:09 +0700

    T3

---
+++ T3/tugas/tugas/home.php
@@ -0,0 +1,12 @@
+<?php
+    include_once 'header.php' ;
+?>
+
+				<h1>Welcome Home!</h1>
+				<p>
+				   Ananda Nuurrussyahidah - 0110122250 - SI05
+				</p>
+			
+<?php
+    include_once 'footer.php' ;
+?>
